27a675c44851d734558d685412efc9bc2694baaa
felipedmsantos <felipe.santos@grupoicts.com.br>
2018-08-23 20:42:27 -0400

    tp2 ok

--- TP2/Forno/Forno.ino
+++ TP2/Forno/Forno.ino
@@ -14,15 +14,31 @@
 #define ledPorta  8
 
 //Botões
-#define botaoDec  9
-#define botaoInc  10
+#define botaoDec  10
+#define botaoInc  9
 #define botaoPorta  13
 #define botaoPower  12
 
 //Buzzer
 #define buzzer 11
 
+//Variáveis para guardar estados dos botões
 
+boolean power = LOW; //ler o estado do botão power
+boolean turnOn = false; //guardar o estado do botão power
+boolean atualPower = LOW;
+
+boolean porta = LOW;
+boolean portaAberta = true;
+boolean atualPorta = LOW;
+
+boolean inc = LOW;
+boolean atualInc = LOW;
+boolean dec = LOW;
+boolean atualDec = LOW;
+
+//tempo exibido no display
+int tempo = 0;
 
 void setup()
 {
@@ -140,10 +156,107 @@ void display(int num)
         break;
   }
 }
+
+boolean debounce(boolean last, int switchPin)
+{
+  boolean current = digitalRead(switchPin);
+  if (last != current)
+  {
+    delay(5);
+    current = digitalRead(switchPin);
+  }
+  return current;
+}
+
+//Verificação de incremento e decremento antes da porta fechar  
+void incDec(){
   
+    atualInc = debounce(inc,botaoInc);
+    if(inc == LOW && atualInc == HIGH)
+    {
+      tempo++;
+        if(tempo > 9)
+          tempo = 9;
+        display(tempo);
+    }
+    inc = atualInc;
+    
+    atualDec = debounce(dec,botaoDec);
+    if(dec == LOW && atualDec == HIGH)
+    {
+      tempo--;
+        if(tempo < 0)
+          tempo = 0;    
+        display(tempo);
+    }
+    dec = atualDec;
+  
+}
+
+void verificaPorta()
+{
+  //Verficação porta aberta
+   atualPorta = debounce(porta, botaoPorta);  
+    if(porta == LOW && atualPorta == HIGH)
+    {
+      portaAberta = !portaAberta; 
+    }
+    porta = atualPorta;
+    digitalWrite(ledPorta, portaAberta);
+  
+}
+
+void fornoLigado()
+{ 
+    //int set = tempo;
+    while(tempo >= 0 && !portaAberta)
+    { 
+        verificaPorta();
+      display(tempo);
+        delay(1000);
+        tempo--;
+    }
+   if(!portaAberta){
+    //tempo = set;
+    digitalWrite(buzzer, HIGH);
+    delay(1000);
+    digitalWrite(buzzer, LOW);
+   }
+    turnOn = LOW;//Desliga o forno depois de terminar
+}
+
 
 void loop()
-{
+{  
+    display(tempo);
+    verificaPorta();    
   
+    if(!turnOn)
+      incDec();
+     
+    if(!portaAberta){
+    
+        if(!turnOn)
+          incDec();
+      //Verificação do botao Power
+      atualPower = debounce(power, botaoPower);  
+      if(power == LOW && atualPower == HIGH)
+      {
+        turnOn = !turnOn; 
+      }
+      power = atualPower;
+      digitalWrite(ledPower, turnOn);
+      if(turnOn && !portaAberta)
+        {
+          fornoLigado();
+        }
+   }
+   else
+   {
+      digitalWrite(ledPower, LOW);
+   }
+     
+    
+    
   
 }
